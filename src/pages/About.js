3e57d6c548afdcfe74ab0e479121433e3ab09175
muoegu <kmicah1794@gmail.com>
2022-12-21 23:37:13 +0800

    maika 1st

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,62 @@
-import React, {component} from 'react';
-import '../Uploads/uploads.css';
-import {useState} from 'react';
+import React, { component } from "react";
+import "../Uploads/uploads.css";
+import { useState } from "react";
+import "../App.css";
 
-function ImageUpload(){
-  const [data] = useState();
-  console.log(data)
-    return(
-        <div className="main">
-          <div className="uploadimage">
-            <label htmlFor="imgs">檔案上傳</label>
-          </div>
-            <input id="imgs" type="file" accept="image/png, image/jpeg,.txt,.doc"/>
-        </div>
+function ImageUpload() {
+  const [VLAU, setVLAU] = useState("");
+  const [conasfoa, setconasfoa] = useState("");
+
+  const Maikaload = (event) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      setVLAU(reader.result);
+    };
+    let files = event.target.value;
+
+    if (files.length === 0) return;
+
+    const [file] = event.target.files;
+    reader.addEventListener(
+      "load",
+      () => {
+        // this will then display a text file
+        setconasfoa(reader.result);
+      },
+      false
     );
-}
-export default ImageUpload;
 
+    if (file) {
+      // console.log(typeof file);
+      reader.readAsText(file);
+    }
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file);
+    setconasfoa(reader.result);
+  };
+  console.log(VLAU);
+  return (
+    <div>
+      <head>
+        <meta charset="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css"></link>
+      </head>
+      <body>
+        <div>
+          <input type="file" onChange={Maikaload}></input>
+          <textarea cols="30" rows="20" value={conasfoa}></textarea>
+        </div>
+        <script src="script.js"></script>
+      </body>
+    </div>
+  );
+}
+
+export default ImageUpload;
